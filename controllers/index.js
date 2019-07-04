fe89a286c9fd1f8c8f70591241acc7157407a162
Javier Caballero <javieracj4@gmail.com>
2019-07-04 02:19:40 +0100

    Refactor Auth Routes & Create controllers

---
+++ controllers/index.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const User = require('../models/user');
+
+module.exports = {
+	async uRegister(req, res) {
+		let newUser = {
+			username: req.body.username,
+			fName: req.body.fName,
+			lName: req.body.lName,
+			email: req.body.email
+		};
+
+		if (req.body.isAdmin == 'on') {
+			user.isAdmin = true;
+		}
+		await User.register(newUser, req.body.password, (err, user) => {
+			if (err) {
+				console.log(err.message);
+
+				res.render('error', { error: err, message: err.message });
+			} else {
+				passport.authenticate('local')(req, res, () => {
+					res.redirect('/');
+				});
+			}
+		});
+	},
+	uLogin(req, res, next) {
+		passport.authenticate('local', {
+			successRedirect: '/',
+			failureRedirect: '/'
+		})(req, res, next);
+	},
+	uLogout(req, res) {
+		req.logOut();
+		console.log('Logged you out!');
+		res.redirect('/');
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -2,54 +2,20 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const passport = require('passport');
-
+const { asyncErrorHandler } = require('../middleware/index');
+const { uRegister, uLogin, uLogout } = require('../controllers/index');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', { currentUser: req.isAuthenticated() });
 });
 
 /* POST register. */
-router.post('/register', (req, res) => {
-	let user = {
-		username: req.body.username,
-		fName: req.body.fName,
-		lName: req.body.lName,
-		email: req.body.email
-	};
-
-	if (req.body.isAdmin == 'on') {
-		user.isAdmin = true;
-	}
-	User.register(new User(user), req.body.password, (err, user) => {
-		if (err) {
-			console.log(err.message);
-
-			res.redirect('/');
-		} else {
-			passport.authenticate('local')(req, res, () => {
-				res.redirect('/');
-			});
-		}
-	});
-});
+router.post('/register', asyncErrorHandler(uRegister));
 
 /* POST /login */
-router.post(
-	'/login',
-	passport.authenticate('local', {
-		successRedirect: '/',
-		failureRedirect: '/'
-	}),
-	(req, res) => {
-		console.log('Logged in Successfully!');
-	}
-);
+router.post('/login', uLogin);
 
 /* GET /logout */
-router.get('/logoff', (req, res) => {
-	req.logOut();
-	console.log('Logged you out!');
-	res.redirect('/');
-});
+router.get('/logout', uLogout);
 
 module.exports = router;
