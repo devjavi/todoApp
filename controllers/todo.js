d3991a01e9ce57033a18f869a7a2687de8ed5ea7
Javier Caballero <javieracj4@gmail.com>
2019-07-06 04:46:34 +0100

    Refactor Index page. Set todos.ejs components

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,5 +1,3 @@
-const mongoose = require('mongoose');
-const passport = require('passport');
 const User = require('../models/user');
 const Todo = require('../models/todo');
 
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,9 @@ const { todoNew } = require('../controllers/todo');
 
 /* GET home/show page. */
 router.get('/', (req, res, next) => {
+	//Check if request is authenticated
 	if (req.isAuthenticated()) {
+		//If true, Populate todos for the user and set currentUser to the user making the request
 		User.findById(req.user.id).populate('todos').exec((err, foundUser) => {
 			if (err) {
 				console.log(err);
@@ -15,8 +17,9 @@ router.get('/', (req, res, next) => {
 				res.render('index', { currentUser: foundUser });
 			}
 		});
+		//Otherwise set currentUser to false to show authForms
 	} else {
-		res.render('index', { currentUser: req.user });
+		res.render('index', { currentUser: false });
 	}
 });
 
